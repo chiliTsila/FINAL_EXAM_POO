d2867065949454f050c4a4ed858d561ab1753576
chiliTsila <hei.chili.4@gmail.com>
2025-10-06 11:51:56 +0300

    modification en ajouttant before each dans NormalUserTest

--- examen_poo/src/test/java/UsersTest.java
+++ examen_poo/src/test/java/UsersTest.java
@@ -1,3 +1,29 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.examen_poo.AnonymousUser;
+import com.examen_poo.NormalUser;
+
 public class UsersTest {
-    
+    private NormalUser user;
+    private AnonymousUser anonyme;
+
+    @BeforeEach
+    public void setUp() {
+        user = new NormalUser(1, "Alice", "Dupont", "alice@example.com");
+        anonyme = new AnonymousUser(99);
+    }
+
+    @Test
+    public void testNormalUserCreation() {
+        assertEquals(1, user.getID());
+        assertEquals("Alice", user.getFirstName());
+    }
+
+    @Test
+    public void testAnonymousUserCreation() {
+        assertEquals(99, anonyme.getID());
+    }
 }
